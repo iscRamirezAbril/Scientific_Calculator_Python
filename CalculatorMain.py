e7f7a6f58db0065f0de5e39aa431b62b3784b77b
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-01-26 19:26:30 -0800

    Format and Color changes

--- CalculatorMain.py
+++ CalculatorMain.py
@@ -1,5 +1,13 @@
+# This is the library that will help with text color.
 from colorama import init, Fore
-init()
+init(autoreset=True)
+
 print(Fore.CYAN + "=============================================================================================")
-print(Fore.CYAN + "========================= Welcome to the Scientific Calculator! :D ==========================")
+print(Fore.CYAN + "------------------------- Welcome to the Scientific Calculator! :D --------------------------")
 print(Fore.CYAN + "=============================================================================================")
+
+# Principal Menu
+print("This is the principal menu.")
+print("a. Basic Math Functions")
+print("b. More Math Functions")
+print("c. Trigonometric Functions")
