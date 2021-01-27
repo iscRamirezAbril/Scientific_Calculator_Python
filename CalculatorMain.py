46a5891bf163596f2dad598c182aa02c27db3f10
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-01-26 20:29:14 -0800

    Function Changes

--- CalculatorMain.py
+++ CalculatorMain.py
@@ -1,9 +1,11 @@
+import os
+
 # This is the library that will help with text color.
 from colorama import init, Fore
 init(autoreset=True)
 
 print(Fore.CYAN + "=============================================================================================")
-print(Fore.CYAN + "                     --- Welcome to the Scientific Calculator! :D ---")
+print(Fore.CYAN + "                     --- Welcome to the Scientific Calculator! :D ---                        ")
 print(Fore.CYAN + "=============================================================================================")
 
 # Principal Menu
@@ -15,3 +17,13 @@ print("d. Inverse Math Functions")
 print("e. Exit")
 
 Option = input("\nPlease, select an option: ")
+
+# Secondary Menu
+if Option == "a": 
+    os.system("cls") # Clear
+    print("a. Basic Math Functions")
+    print("   1. Addition")
+    print("   2. Sustraction")
+    print("   3. Multiplication")
+    print("   4. Division")
+elif Option == "b"
