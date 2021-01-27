55e6519335c9588a049081ecb4501076e4bac5fb
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-01-27 12:48:30 -0800

    Options Menu changes

--- CalculatorMain.py
+++ CalculatorMain.py
@@ -4,6 +4,7 @@ import os
 from colorama import init, Fore
 init(autoreset=True)
 
+os.system("cls") # Clear the window
 print(Fore.CYAN + "=============================================================================================")
 print(Fore.CYAN + "                     --- Welcome to the Scientific Calculator! :D ---                        ")
 print(Fore.CYAN + "=============================================================================================")
@@ -20,10 +21,17 @@ Option = input("\nPlease, select an option: ")
 
 # Secondary Menu
 if Option == "a": 
-    os.system("cls") # Clear
+    os.system("cls") # Clear the window
     print("a. Basic Math Functions")
     print("   1. Addition")
-    print("   2. Sustraction")
+    print("   2. Subtraction")
     print("   3. Multiplication")
     print("   4. Division")
 elif Option == "b"
+    os.system("cls") # Clear the window
+    print("b. More Math Functions")
+    print("   1. Square of a number")
+    print("   2. Cube of a number")
+    print("   3. Number raised to the 10 power")
+    print("   4. Percentage of a number")
+    
