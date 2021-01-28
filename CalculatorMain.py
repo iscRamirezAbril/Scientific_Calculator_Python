849ef5441af4767e3f0326f6aeb7e73ee566e8fe
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-01-27 20:39:39 -0800

    Menu Changes

--- CalculatorMain.py
+++ CalculatorMain.py
@@ -87,20 +87,77 @@ while(contador == 1):
             # ---------------------------------------- #
             Option = int(input("C. TRIGONOMETRIC FUNCTIONS\n   1. Basic Trigonometric Functions\n   2. Inverse Trigonometric Functions\n   3. Hiperbolic Trigonometric Functions\n   4. Return\n\nPlease, select an option: "))
             if Option == 1:
-                print("Pending...")
+                contador2 = 0
+                os.system("cls") # Clear the window
+                while(contador2 == 0):
+                    # ---------------------------------------- #
+                    # ----- Trigonometric Functions Menu ----- #
+                    # ---------------------------------------- #
+                    Option = int(input("I. BASIC TRIGONOMETRIC FUNCTIONS\n   1. Sine [sin(x)]\n   2. Cosine [cos(x)]\n   3. Tangent [tan(x)]\n   4. Secant [sec(x)]\n   5. Cosecant [csc(x)]\n   6. Cotangent [cot[x]]\n   7. Return\n\nPlease, select an option: "))
+                    if Option == 1:
+                        print("Pending...")
+                    elif Option == 2:
+                        print("Pending...")
+                    elif Option == 3:
+                        print("Pending...")
+                    elif Option == 4:
+                        print("Pending...")
+                    elif Option == 5:
+                        print("Pending...")
+                    elif Option == 6:
+                        print("Pending...")
+                    elif Option == 7:
+                        print("Press any Key to return to the Trigonometric Functions Menu...")
+                        msvcrt.getwch() # Press a key before returning to the main menu
+                        break
+                os.system("cls") # Clear the window
             elif Option == 2:
-                print("Pending...")
+                contador2 = 0
+                os.system("cls") # Clear the window
+                while(contador2 == 0):
+                    # ------------------------------------------------ #
+                    # ----- Inverse Trigonometric Functions Menu ----- #
+                    # ------------------------------------------------ #
+                    Option = int(input("II. INVERSE TRIGONOMETRIC FUNCTIONS\n   1. Arcsin(x)\n   2. Arccos(x)\n   3. Arctan(x)\n   4. Return\n\nPlease, select an option: "))
+                    if Option == 1:
+                        print("Pending...")
+                    elif Option == 2:
+                        print("Pending...")
+                    elif Option == 3:
+                        print("Pending...")
+                    elif Option == 4:
+                        print("Press any Key to return to the Trigonometric Functions Menu...")
+                        msvcrt.getwch() # Press a key before returning to the main menu
+                        break
             elif Option == 3:
-                print("Pending...")
+                contador2 = 0
+                os.system("cls") # Clear the window
+                while(contador2 == 0):
+                    # --------------------------------------------------- #
+                    # ----- Hiperbolic Trigonometric Functions Menu ----- #
+                    # --------------------------------------------------- #
+                    Option = int(input("III. HIPERBOLIC TRIGONOMETRIC FUNCTIONS\n   1. sinh(x)\n   2. cosh(x)\n   3. tanh(x)\n   4. Return\n\nPlease, select an option: "))
+                    if Option == 1:
+                        print("Pending...")
+                    elif Option == 2:
+                        print("Pending...")
+                    elif Option == 3:
+                        print("Pending...")
+                    elif Option == 4:
+                        print("Press any Key to return to the Trigonometric Functions Menu...")
+                        msvcrt.getwch() # Press a key before returning to the main menu
+                        break
             elif Option == 4:
                 print("Press any Key to return to the Main Menu...")
                 msvcrt.getwch() # Press a key before returning to the main menu
                 break
         os.system("cls") # Clear the window
+
     elif(Option == 4):
         print("Thanks for using this program! See ya soon! n.n/")
         msvcrt.getwch() # Press a key to close the program
         sys.exit() # Close the window
+
     else:
         print("Please, select a valid option...")
         print("Press any key to try again :)")
